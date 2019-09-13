1451983ffebd121ac49da2f818adc09e53887abf
changSic <a920506@gmail.com>
2019-09-14 00:19:40 +0900

    Add MaximumNum

--- Programmers/Gymcloth_Greedy.py
+++ Programmers/Gymcloth_Greedy.py
@@ -18,6 +18,7 @@ Created on Wed Sep 11 23:48:24 2019
 # (1) lost 에 있는 아이가 reserve에 있다면 일반 학생으로 바꾸기 (배열에서 지우기)
 # (2) lost 에 있는 아이를 기준으로 작은 학생 껏 부터 빌림을 시도해서 빌리면 배열에서 지우기.
 
+
 def solution(n, lost, reserve):
     if n < 2 or n > 30 or len(reserve) < 1 or len(reserve) > n or len(lost) < 1 or len(lost) > n :
         return -1
@@ -28,7 +29,7 @@ def solution(n, lost, reserve):
             lost.remove(i)
             reserve.remove(i)
             
-    # (2)
+    # (2) 
     for i in lost[:]:
         if i - 1 in reserve:
             lost.remove(i)
@@ -37,7 +38,7 @@ def solution(n, lost, reserve):
             lost.remove(i)
             reserve.remove(i + 1)
             
-    # (3)4
+    # (3)
     answer = n - len(lost)
     
     print("lost is : ",lost," reserve is : ", reserve, " answer is : ", answer)
@@ -63,3 +64,43 @@ def solution2(n, lost, reserve):
         elif b in _lost:
             _lost.remove(b)
     return n - len(_lost)
+
+
+## 강의 풀이
+    
+# 방법 1 (n 이 작을 때)
+def solution3(n, lost, reserve):
+    uniform = [1] * (n + 2) # 체육복 모두 1로 초기화 하고 인덱스 맨 앞에, 맨 뒤에 여분의 하나씩 더 해서 리스트의 접근 쉽게 할 것.
+    
+    for i in reserve: # 빌려줄 수 있는 학생들
+        uniform[i] += 1
+    
+    for i in lost: # 도난 당한 학생들 
+        uniform[i] -= 1
+        
+    for i in range(1,n + 1): # 빌려주는 동작
+        if uniform[i - 1] == 0 and uniform[i] == 2: # 왼쪽 학생 먼저 빌려줄지 확인하고
+            uniform[i - 1:i + 1] = [1, 1]
+        elif uniform[i] == 2 and uniform[i + 1] == 0: # 오른쪽 학생 빌려주질 확인
+            uniform[i:i + 2] = [1, 1]
+    
+    return len([x for x in uniform[1:-1] if x > 0])
+    
+
+# 방법 2 (n 이 클 때) 집합 사용
+def solution4(n, lost, reserve):
+    s = set(lost) & set(reserve) # &는 교집합 / 여분도 있고 도난도 당한 애들 
+    l = set(lost) - s # 실제로 체육복을 빌려야 하는 학생들
+    r = set(reserve) - s # 실제로 빌려줄 수 있는 학생들
+    
+    for x in sorted(r): 
+        if x - 1 in l:
+            l.remove(x - 1)
+        elif x + 1 in l:
+            l.remove(x + 1)
+    return n - len(l)
+    
+    
+    
+    
+    
--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 * 문제를 분석하고 어떤 자료구조나 알고리즘을 사용할지 판단한다.
 
+탐욕법
+알고리즘의 각 단계에서
+그 순간에 최적이라고 생각되는 것을 선택.
+현재의 선택이 마지막 해답의 최적성을 해치지 않을 때 (지금 좋은게 끝에도 좋을 때) 탐욕법 사용 가능
 ## Programmers
 | 출처 | 문제 | 카테고리 | 내 풀이 + 인기 풀이 |  
 | ------------- | ------------- | ------------- | ------------- |
 | Programmers | [체육복](https://programmers.co.kr/learn/courses/30/lessons/42862) | 탐욕법 | [![](images/python_logo.png?raw=true)](Programmers/Gymcloth_Greedy.py) |
 | Programmers | [완주하지 못한 선수](https://programmers.co.kr/learn/courses/30/lessons/42576) | 해시 | [![](images/python_logo.png?raw=true)](Programmers/Marathon_Hash.py) |
+| Programmers | [가장 큰 수](https://programmers.co.kr/learn/courses/30/lessons/42746) | 정렬 | [![](images/python_logo.png?raw=true)](Programmers/MaximumNumber_Sort.py) |
